3bca7031455e42cc9b5235aac5b6bf9236b98dff
Lepore <LeporeM@SI.EDU>
2017-11-17 11:41:09 -0500

    Adds to #3: Improve navigating file, with Shift+Alt+J or Shift+Control+O.
    * Replace section brakes from #### to 3 line-brakes (white spece).

--- data-raw/from_Graham_Zemunik/from_Graham_Zemunik/Kriging functions.R
+++ data-raw/from_Graham_Zemunik/from_Graham_Zemunik/Kriging functions.R
@@ -1,10 +1,10 @@
-##################################
 # Kriging functionality for CTFS
 #
 # At the top level, simply calling the function GetKrigedSoil will calculate
 # a "best" semivariogram to use and krige the soil data without any other input
 
-##################
+
+
 # GetKrigedSoil
 #
 # Kriges the soil data from "df.soil" using soil variable "var",
@@ -13,7 +13,7 @@
 # By default the best kriging parameters found via the geoR variogram
 # function are used, but specified parameters can by used via the
 # "krigeParams" argument
-# geoR has two main kriging functions: ksLine and krige.conv. The 
+# geoR has two main kriging functions: ksLine and krige.conv. The
 # argument "useKsLine" specifies whether to use the ksline function or not.
 #
 # Parameters:
@@ -35,14 +35,14 @@
 #   lambda: the "lambda" value used in the Box-Cox transform of the raw data
 #   vg: the variogram parameters used for the kriging
 #   vm: minimum loss value returned from the geoR variofit function
-GetKrigedSoil <- function( df.soil, var="P", gridSize=20, 
+GetKrigedSoil <- function( df.soil, var="P", gridSize=20,
                            krigeParams=NULL, xSize=1000, ySize=500,
                            breaks=ExpList( 2, 320, 30 ),
                            useKsLine=T )
 {
   df <- df.soil[ , c("gx", "gy", var) ]
   names( df )[ 3 ] <- "z"
-  
+
   # This follows a similar methodology to the John 2007 paper:
   # 1. the data is transformed based on the optimal boxcox transform
   # 2. a polynomial regression curve is fitted - a second order
@@ -51,17 +51,17 @@ GetKrigedSoil <- function( df.soil, var="P", gridSize=20,
   #    get a variogram, b) do the kriging
   # 4. the kriged values are added to the polynomial fit and they
   #    are backtranformed as required
-  
+
   # The lambda for the box-cox transform is restricted to 0, 0.5 and 1.
   # Data with 0's in there are handled by the addition of a small constant
   # in the regression
   bc <- BoxCoxTransformSoil( df )
   df <- bc$df
-  
+
   polyfit <- GetPolynomialFit( df, gridSize=gridSize, xSize=xSize, ySize=ySize )
-  
+
   # Get the variogram parameters
-  # If a polynomial fit was possible then the parameters come from 
+  # If a polynomial fit was possible then the parameters come from
   # the residuals of the polynomial fit
   # Otherwise, the raw data is used (a failure to fit the polynomial
   # most likely indicates that no trend exists, so trend removal isn't
@@ -71,7 +71,7 @@ GetKrigedSoil <- function( df.soil, var="P", gridSize=20,
   } else {
     df.krig <- cbind( polyfit$df.orig[,c("gx","gy")], z=resid( polyfit$mod ) )
   }
-  
+
   require( geoR )
   geod <- as.geodata( df.krig )
   if ( is.null( krigeParams ) ) {
@@ -80,7 +80,7 @@ GetKrigedSoil <- function( df.soil, var="P", gridSize=20,
     params <- krigeParams
     if ( !("kappa" %in% names(params)) ) params$kappa <- 0
   }
-  
+
   # Do the kriging
   if ( useKsLine ) {
     krig <- ksline( geod, locations=polyfit$df.interpolated[,c("gx","gy")],
@@ -98,18 +98,19 @@ GetKrigedSoil <- function( df.soil, var="P", gridSize=20,
   } else {
     df.pred$z <- df.pred$z + krig$predict
   }
-  
+
   # Back transform (if required)
   df.pred <- InvBoxCoxTransformSoil( df.pred, bc$lambda, bc$delta )
-  
+
   names( df.pred ) <- c( "x", "y", "z" )
-  
+
   # Return all useful data
   return( list( df=df.pred, df.poly=polyfit$df.interpolated,
                 lambda=bc$lambda, vg=params$vg, vm=params$minVM ) )
 }
 
-###############
+
+
 # ExpList
 #
 # Returns a list of n values which exponentially
@@ -119,16 +120,17 @@ ExpList <- function( first, last, n )
   v <- vector()
   m <- 1 / (n - 1)
   quotient <- (last / first)^m
-  
+
   v[1] <- first
   for ( i in 2:n ) v[i] <- v[i - 1] * quotient
-  
+
   v
 }
 
-##############
+
+
 # GetAutomatedKrigeParams
-# 
+#
 # Uses the geoR function variofit, with a range of variogram models, to
 # find the "best" variogram parameters for the given geodata object
 #
@@ -138,12 +140,12 @@ ExpList <- function( first, last, n )
 GetAutomatedKrigeParams <- function( geod, trend="cte", breaks=ExpList( 2, 320, 30 ) )
 {
   require(geoR)
-  
+
   # The default breaks argument is set to have more points where the curve
   # rises the most and exponentially fewer at large distances
   # This means that the curve fitting is not overly biased by points
   # beyond the effective maximum range
-  
+
   # Several different models are tested; the one with the lowest least
   # squares error is chosen
   vg <- variog( geod, breaks=breaks, pairs.min=5, trend=trend )
@@ -153,19 +155,20 @@ GetAutomatedKrigeParams <- function( geod, trend="cte", breaks=ExpList( 2, 320, 
   startRange <- max( breaks ) / 2
   initialVal <- max( vg$v ) / 2
   for ( i in 1:length(varModels) ) {
-    vm <- variofit( vg, ini.cov.pars=c( initialVal, startRange ), 
+    vm <- variofit( vg, ini.cov.pars=c( initialVal, startRange ),
                     nugget=initialVal, cov.model=varModels[i] )
     if ( is.null(minValue) || vm$value < minValue ) {
       minValue <- vm$value
       minVM <- vm
     }
   }
-  return( list( nugget=minVM$nugget, sill=minVM$cov.pars[1], 
+  return( list( nugget=minVM$nugget, sill=minVM$cov.pars[1],
                 range=minVM$cov.pars[2], kappa=minVM$kappa, model=minVM$cov.model,
                 minVM=minVM, vg=vg ) )
 }
 
-################
+
+
 # GetPolynomialFit
 #
 # Given a data frame, df, with columns specifying the x, y coordinates
@@ -180,27 +183,28 @@ GetPolynomialFit <- function( df, gridSize=20, xSize=1000, ySize=500 )
 {
   # The data frame is assumed to be x, y, z
   names(df) <- c("gx", "gy", "z")
-  
+
   model <- NULL
-  tryCatch( model <- nls( z ~ PolynomialSurfaceOrder2(gx, gy, a, b, c, d, e, f ), 
-                          data=df, start=list(a=0.1, b=0.1, c=0.1, d=0.1, e=0.1, f=0.1), 
+  tryCatch( model <- nls( z ~ PolynomialSurfaceOrder2(gx, gy, a, b, c, d, e, f ),
+                          data=df, start=list(a=0.1, b=0.1, c=0.1, d=0.1, e=0.1, f=0.1),
                           trace=F, control=nls.control(maxiter=200,minFactor=1/4096) ),
             error = function(e) {} )
-  
+
   halfGrid <- gridSize/2
   df.locations <- expand.grid( gx=seq( halfGrid, xSize-halfGrid, by=gridSize ),
                                gy=seq( halfGrid, ySize-halfGrid, by=gridSize ) )
   if ( !is.null(model) ) df.locations$z <- predict( model, newdata=df.locations )
-  
+
   return( list( df.orig=df, df.interpolated=df.locations, mod=model ) )
 }
 
-################
+
+
 # PolynomialSurfaceOrder2
 #
 # Returns a polynomial 2nd order surface (x,y) defined by the parameters a to f
 PolynomialSurfaceOrder2 <- function( x, y, a, b, c, d, e, f )
-{ 
+{
   a + b*x + c*y + d*x*y + e*x^2 + f*y^2
 }
 
@@ -211,20 +215,20 @@ PolynomialSurfaceOrder2 <- function( x, y, a, b, c, d, e, f )
 # to 0, 0.5 and 1. Only data >=0 can be transformed. Values = 0 are
 # handled by adding a small value, delta, which if used is returned
 # as the delta argument.
-# Returns a list of the original df, the delta value 
+# Returns a list of the original df, the delta value
 #         and the the delta value
 #
 BoxCoxTransformSoil <- function( df )
 {
   lambda <- 1
   delta <- 0
-  
+
   if ( ncol(df) >= 3 ) {
     # Sanity checking and enforcement of the structure
     if ( !identical( names(df)[1:3], c("gx", "gy", "z") ) ) {
       names(df)[1:3] <- c("gx", "gy", "z")
     }
-    
+
     require(MASS)
     if ( min( df$z ) >= 0 ) { # boxcox will complain about -ve values
       if ( min( df$z ) == 0 ) {
@@ -245,7 +249,7 @@ BoxCoxTransformSoil <- function( df )
 }
 
 # InvBoxCoxTransformSoil
-# Performed the inverse of the Box-Cox transform given the data, df, 
+# Performed the inverse of the Box-Cox transform given the data, df,
 # the lambda value and and delta added to the data
 # Return the df with the transformed data, from the z column
 InvBoxCoxTransformSoil <- function( df, lambda, delta )
@@ -255,7 +259,7 @@ InvBoxCoxTransformSoil <- function( df, lambda, delta )
   } else if ( lambda == 0.5 ) df$z <- df$z ^ 2
   # Take away the delta offset
   df$z <- df$z - delta
-  
+
   df
 }
 
