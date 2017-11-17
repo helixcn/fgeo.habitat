5a176ad712faf0d6fb5c58f36f15415397efa35d
Lepore <LeporeM@SI.EDU>
2017-11-17 11:37:25 -0500

    Document data. Adds to #3.

---
+++ R/krig-package.R
@@ -0,0 +1,3 @@
+#' Krig soil data as per John et al. (2007).
+#' @keywords internal
+"_PACKAGE"
---
+++ R/soil_random.R
@@ -0,0 +1,7 @@
+#' Soil data for examples.
+#'
+#' This data set is based on data from Barro Colorado Island, but the variable
+#' `M3Al` has been randomized. It is useful for examples but not for research.
+#'
+#' @source Graham Zemunik (via https://goo.gl/2EwLQJ).
+"soil_random"
