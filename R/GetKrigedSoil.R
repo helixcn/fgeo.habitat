6443dcbb5ac267deeb83aee3f892cb2f8149b2f5
Lepore <LeporeM@SI.EDU>
2017-11-17 12:32:31 -0500

    Adds to #3: Add examples to main function.

--- R/GetKrigedSoil.R
+++ R/GetKrigedSoil.R
@@ -35,7 +35,28 @@
 #'   [geoR::ksline()], [geoR::krige.conv()], [geoR::krige.control()].
 #'
 #' @examples
+#' \dontrun{
+#' # Use randomized data set for examples (invalid for reaserch).
+#' df <- krig::soil_random
+#' str(df)
 #'
+#' # Krige with automated parameters
+#' kriged <- GetKrigedSoil( df, var="M3Al" )
+#' str(kriged)
+#'
+#' # Krige with specified parameters - these example params are rather arbitrary
+#' # but are loosely based on what was chosen from the automated kriging
+#' params <- list( model="circular", range=100, nugget=1000, sill=46000, kappa=0.5 )
+#' kriged.2 <- GetKrigedSoil( df, var="M3Al", krigeParams=params )
+#'
+#' # Have a look at the differences
+#' g <- ggplot( kriged$df, aes( x=x, y=y, fill=z ) )
+#' g <- g + geom_tile() + coord_equal()
+#' g2 <- ggplot( kriged.2$df, aes( x=x, y=y, fill=z ) )
+#' g2 <- g2 + geom_tile() + coord_equal()
+#' g
+#' g2
+#' }
 #' @export
 GetKrigedSoil <- function(df.soil,
                           var = "P",
