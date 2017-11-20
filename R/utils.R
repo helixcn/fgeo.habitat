7094f926dd22848b778bd64f24a3ee5274ca4197
maurolepore <maurolepore@gmail.com>
2017-11-20 15:29:15 -0500

    Update @imports to avoid NOTE of check().

--- R/utils.R
+++ R/utils.R
@@ -2,6 +2,9 @@
 
 #' @import geoR
 #' @importFrom MASS boxcox
+#' @importFrom stats nls
+#' @importFrom stats rnorm
+#' @importFrom stats var
 #' @importFrom stats nls.control
 #' @importFrom stats predict
 #' @importFrom stats resid
