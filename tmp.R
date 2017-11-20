95ade72ddae475a4afe22f3ccbd4cacea9f2e438
maurolepore <maurolepore@gmail.com>
2017-11-20 13:28:51 -0500

    update tmp.R

--- tmp.R
+++ tmp.R
@@ -1,16 +1,3 @@
-op <- options(warn = 2)
-options("warn")
-
 library(krig)
 df <- krig::soil_random
 GetKrigedSoil( df, var="M3Al" )
-
-options(op)
-
-
-
-# xxxcont
-
-* check with other `m0` arguments.
-* confirm regression tests.
-* close issue
