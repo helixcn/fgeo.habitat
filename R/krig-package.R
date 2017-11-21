3e599293df176ce96ce29b58411c657bf37b0d5b
maurolepore <maurolepore@gmail.com>
2017-11-21 16:14:40 -0500

    Move contents of utils to pkg documentation.
    This is the approach of the tidyverse. It reduces the number of files.

--- R/krig-package.R
+++ R/krig-package.R
@@ -1,2 +1,15 @@
 #' @keywords internal
 "_PACKAGE"
+
+# Suppress R CMD check note
+
+#' @import geoR
+#' @importFrom MASS boxcox
+#' @importFrom stats nls
+#' @importFrom stats rnorm
+#' @importFrom stats var
+#' @importFrom stats nls.control
+#' @importFrom stats predict
+#' @importFrom stats resid
+NULL
+
