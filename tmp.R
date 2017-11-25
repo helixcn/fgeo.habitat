657d3585ef7c3595ecc19ab5ec5f2ca91c31893f
maurolepore <maurolepore@gmail.com>
2017-11-25 00:43:13 -0500

    Draft app.

--- tmp.R
+++ tmp.R
@@ -1,3 +1,30 @@
-library(krig)
+library(ggplot2)
+
+# Use randomized data set for examples (invalid for reaserch).
 df <- krig::soil_random
-GetKrigedSoil(df, var = "M3Al")
+head(df)
+
+# Krige with automated parameters
+kriged <- GetKrigedSoil(df, var = "M3Al")
+str(kriged)
+
+# Krige with specified parameters - these example params are rather arbitrary
+# but are loosely based on what was chosen from the automated kriging
+params <- list(
+  model = "circular",
+  range = 100,
+  nugget = 1000,
+  sill = 46000,
+  kappa = 0.5
+)
+kriged.2 <- GetKrigedSoil(df, var = "M3Al", krigeParams = params)
+
+# Have a look at the differences
+g <- ggplot(kriged$df, aes(x = x, y = y, fill = z)) +
+  geom_tile() +
+  coord_equal()
+g2 <- ggplot(kriged.2$df, aes(x = x, y = y, fill = z)) +
+  geom_tile() +
+  coord_equal()
+g
+g2
