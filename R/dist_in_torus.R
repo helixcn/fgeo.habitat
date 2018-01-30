0b510c1cbf4d87c37071a592c0569757eb91c232
maurolepore <maurolepore@gmail.com>
2018-01-30 18:46:43 -0500

    Draft questions for Gabriel.

--- R/dist_in_torus.R
+++ R/dist_in_torus.R
@@ -3,7 +3,7 @@
 #' By default, this function returns the distance in the Euclidean space
 #' by assuming borders infinitely apart (i.e. points in a small portion
 #' of an infinitely large toroid). 
-#' [xxx What happens if you change the default?]
+#' \[xxx What happens if you change the default?\]
 #' 
 #' The shortest distance in the toroid is the hypotenuse of the smallest
 #' hyper-triangle. The 'internal' distance is the typical distance based on the
@@ -11,13 +11,13 @@
 #' borders, going around. There are only two ways of measuring distance along
 #' each dimension.
 #' 
-#' @param x The matrix with the coordinates of the points. [xxx The data
+#' @param x The matrix with the coordinates of the points. \[xxx The data
 #'   sould contain exclusively the coordinates, right? If so it may be better
 #'   to restrict the data to a matrix -- dataframes can mix vectors of different
-#'   types which may fail.]
+#'   types which may fail.\]
 #' @param lower,upper The minimum and maximum possible values of the coordinates
-#'   along each dimension. [xxx should these always be vectors of length 2? --
-#'   as shown in the example?]
+#'   along each dimension. \[xxx should these always be vectors of length 2? --
+#'   as shown in the example?\]
 #'   
 #' @author Gabriel Arellano
 #'
@@ -25,7 +25,7 @@
 #' @export
 #'
 #' @examples
-#' # [xxx can you add] an example in more than 2 dimensions?]
+#' # \[xxx can you add\] an example in more than 2 dimensions?\]
 #' 
 #' x <- data.frame(runif(10, min = 3, max = 5), runif(10, min = 13, max = 15))
 #' 
@@ -44,7 +44,7 @@
 #' abline(0, 1)
 #' 
 #' 
-#' # [xxx Is this behaviour OK?]
+#' # \[xxx Is this behaviour OK?\]
 #' # # Passes
 #' # dist_in_torus(matrix(c(NaN, NaN)))
 #' # dist_in_torus(matrix(c(NA, NA)))
@@ -58,7 +58,7 @@
 #' # dist_in_torus(matrix(a = c(1)))
 #' # dist_in_torus(matrix(c(NULL, NULL)))
 #' # 
-#' # # [xxx What known outputs would be useful for tests? For example:
+#' # # \[xxx What known outputs would be useful for tests? For example:
 #' # library(testthat)
 #' # v1 <- c(0, 1, 1, 0)
 #' # expect_equal(c(dist_in_torus(matrix(c(1, 2)))), v1)
