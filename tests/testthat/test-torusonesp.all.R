ef41c763340648e88eab76e18c72d86230cb069c
maurolepore <maurolepore@gmail.com>
2018-04-24 19:17:42 -0400

    #23: Improve tests:
    * Stress tests with 1, 2 and 3 sp from Luquillo.
    * Clean less important tests

--- tests/testthat/test-torusonesp.all.R
+++ tests/testthat/test-torusonesp.all.R
@@ -11,28 +11,39 @@ sp_top3 <- cns_luq %>%
   arrange(desc(n)) %>%
   top_n(3) %>%
   pull(sp)
-cns_3sp_luq <- filter(cns_luq, status == "A", sp  %in% sp_top3)
-this_sp_luq <- first(sp_top3)
 hab_luq <- fgeo.data::luquillo_habitat
 pdim_luq <- c(320, 500)
 gsize_luq <- 20
+this_sp_luq <- first(sp_top3)
 
-abun_quad_luq <- abundanceperquad(
-  cns_3sp_luq, plotdim = pdim_luq, gridsize = gsize_luq, type = 'abund'
-)$abund
-
-test_that("works with luquillo", {
+abundance_sp <- function(n) {
+  .cns <- filter(cns_luq, status == "A", sp  %in% sp_top3[1:n])
+  abundanceperquad(
+    .cns, plotdim = pdim_luq, gridsize = gsize_luq, type = 'abund'
+  )$abund
+}
+expect_silent_with_n <- function(n) {
   expect_silent({
-    out <- torusonesp.all(
+    torusonesp.all(
       species = this_sp_luq,
       hab.index20 = hab_luq,
-      allabund20 = abun_quad_luq,
+      allabund20 = abundance_sp(n),
       plotdim = pdim_luq,
       gridsize = gsize_luq
     )
   })
+}
+test_that("works with luquillo", {
+  out <- expect_silent_with_n(3)
   expect_known_output(out, "ref-luq_top3_premon", print = TRUE, update = TRUE)
 })
+
+test_that("outputs silently with a 1- and 2- species dataset from Luquillo", {
+  expect_silent_with_n(1)
+  expect_silent_with_n(2)
+})
+
+
 
 # Pasoh -------------------------------------------------------------------
 
@@ -51,15 +62,11 @@ test_that("tests with Pasoh", {
     sample_n(50) %>% 
     ungroup() 
   hab <- pasoh::pasoh_hab_index20
-
-  
   
   # REGRESSION
-  
   abun_quad <- abundanceperquad(
     cns_tiny, plotdim = pdim, gridsize = gsize, type = 'abund'
   )$abund
-  
   out_onesp <- torusonesp.all(
     species = this_sp,
     hab.index20 = hab,
@@ -69,24 +76,17 @@ test_that("tests with Pasoh", {
   )
   # Transpose for better display
   one_sp_pasoh <- t(out_onesp)
-  
   expect_known_output(
     one_sp_pasoh, "ref-one_sp_n50_pasoh", print = TRUE, update = TRUE
   )
 
-
-  
   # FAILS WITH A ONE-SPECIES DATASET
-  
   # If datasets has only one species, the test fails -- this makes sense but
   # should confirm with Russo et al
-  cns_1sp <- cns_tiny %>% 
-    filter(sp  == this_sp)
-
+  cns_1sp <- cns_tiny %>% filter(sp  == this_sp)
   abun_quad <- abundanceperquad(
     cns_1sp, plotdim = pdim, gridsize = gsize, type = 'abund'
   )$abund
-  
   expect_warning(
     torusonesp.all(
       species = this_sp,
@@ -98,17 +98,11 @@ test_that("tests with Pasoh", {
     "Values can't be compared:"
   )
   
-  
-  
   # PASSES WITH A TWO-SPECIES DATASET
-  
-  cns_2sp <- cns_tiny %>% 
-    filter(sp %in% sample(cns_tiny$sp, 2))
-  
+  cns_2sp <- cns_tiny %>% filter(sp %in% sample(cns_tiny$sp, 2))
   abun_quad <- abundanceperquad(
     cns_2sp, plotdim = pdim, gridsize = gsize, type = 'abund'
   )$abund
-  
   expect_silent(
     torusonesp.all(
       species = this_sp,
@@ -119,72 +113,3 @@ test_that("tests with Pasoh", {
     )
   )
 })
-
-
-
-
-# BCI ---------------------------------------------------------------------
-
-test_that("passes with different habitat datasets from BCI", {
-  skip_if_not_installed("bci")
-  skip_if_not_installed("bciex")
-  
-  # Small dataset from BCI
-  cns_bci <- bciex::bci12t7mini
-  sp_top3 <- cns_bci %>%
-    count(sp) %>%
-    arrange(desc(n)) %>%
-    top_n(3) %>%
-    pull(sp)
-  
-  cns_3sp_bci <- filter(cns_bci, status == "A", sp  %in% sp_top3)
-  this_sp_bci <- first(sp_top3)
-  
-  pdim_bci <- c(1000, 500)
-  gsize_bci <- 20
-
-  abun_quad_bci <- abundanceperquad(
-    cns_3sp_bci, plotdim = pdim_bci, gridsize = gsize_bci, type = 'abund'
-  )$abund
-  
-  # With bci, all is good
-  expect_silent(
-    torusonesp.all(
-      species = this_sp_bci,
-      hab.index20 = bci::bci_habitat,
-      allabund20 = abun_quad_bci,
-      plotdim = pdim_bci,
-      gridsize = gsize_bci
-    )
-  )
-  
-  # With bciex, which habitat data is based from elevation data, it's unclear
-  # why there are many habitats with cero species.
-  hab_bciex <- rename(bciex::bci_habitat, habitats = habitat) %>%
-    mutate(habitats = as.integer(as.factor(habitats)))
-  expect_warning(
-    torusonesp.all(
-      species = this_sp_bci,
-      hab.index20 = hab_bciex,
-      allabund20 = abun_quad_bci,
-      plotdim = pdim_bci,
-      gridsize = gsize_bci
-    )
-  )
-  
-  # With randomly generated habitats, all is good.
-  hab_random <- mutate(
-    hab_bciex, habitats = sample(1:7, length(habitats), replace = TRUE)
-  )
-  expect_silent(
-    torusonesp.all(
-      species = this_sp_bci,
-      hab.index20 = hab_random,
-      allabund20 = abun_quad_bci,
-      plotdim = pdim_bci,
-      gridsize = gsize_bci
-    )
-  )
-})
-
-
