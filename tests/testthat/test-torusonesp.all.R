b1206f4af6c76dae313e043650f7e70f3e494e6d
maurolepore <maurolepore@gmail.com>
2018-04-24 09:11:12 -0400

    #23: Add tests and reduce duplication.

--- tests/testthat/test-torusonesp.all.R
+++ tests/testthat/test-torusonesp.all.R
@@ -1,34 +1,36 @@
 context("torusonesp.all.R")
 
+# Ensure consistent values accross runs
+set.seed(123)
+
 library(fgeo.habitat)
 library(dplyr)
+
+# Make a tiny dataset to run test fast
+cns <- pasoh::pasoh_3spp
+cns_tiny <- cns %>% 
+  as_tibble() %>% 
+  filter(status == "A") %>% 
+  group_by(sp) %>% 
+  sample_n(50) %>% 
+  ungroup() 
+
+hab <- pasoh::pasoh_hab_index20
+pdim <- c(1000, 500)
+gsize <- 20
+this_sp <- "GIROPA"
+
+
 
 # Regression --------------------------------------------------------------
 
 test_that("regression with Pasoh", {
-  # Setup
   skip_if_not_installed("pasoh")
-  set.seed(123)
-  total_abundance <- 50
-  cns <- pasoh::pasoh_3spp
-  cns_n50 <- cns %>% 
-    as_tibble() %>% 
-    filter(status == "A") %>% 
-    group_by(sp) %>% 
-    sample_n(total_abundance) %>% 
-    ungroup() 
-  hab <- pasoh::pasoh_hab_index20
-  pdim <- c(1000, 500)
-  gsize <- 20
 
   abun_quad <- abundanceperquad(
-    cns_n50,
-    plotdim = pdim,
-    gridsize = gsize,
-    type = 'abund'
+    cns_tiny, plotdim = pdim, gridsize = gsize, type = 'abund'
   )$abund
   
-  this_sp <- "GIROPA"
   out_onesp <- torusonesp.all(
     species = this_sp,
     hab.index20 = hab,
@@ -36,42 +38,30 @@ test_that("regression with Pasoh", {
     plotdim = pdim,
     gridsize = gsize
   )
+  # Transpose for better display
   one_sp_pasoh <- t(out_onesp)
   
   expect_known_output(
-    one_sp_pasoh,
-    "ref-one_sp_n50_pasoh",
-    print = TRUE,
-    update = TRUE
+    one_sp_pasoh, "ref-one_sp_n50_pasoh", print = TRUE, update = TRUE
   )
 })
 
 
 
-# Too few species ---------------------------------------------------------
+# Minimum number of species in census -------------------------------------
 
 test_that("fails with a one-species dataset", {
-  # Setup
   skip_if_not_installed("pasoh")
-  total_abundance <- 50
-  cns_too_few_sp <- pasoh::pasoh_3spp %>% 
-    filter(sp  == "GIROPA") %>% 
-    filter(status == "A") %>% 
-    group_by(sp) %>% 
-    sample_n(total_abundance) %>% 
-    ungroup() 
-  hab <- pasoh::pasoh_hab_index20
-  pdim <- c(1000, 500)
-  gsize <- 20
   
+  # If datasets has only one species, the test fails -- this makes sense but
+  # should confirm with Russo et al
+  cns_1sp <- cns_tiny %>% 
+    filter(sp  == this_sp)
+
   abun_quad <- abundanceperquad(
-    cns_too_few_sp,
-    plotdim = pdim,
-    gridsize = gsize,
-    type = 'abund'
+    cns_1sp, plotdim = pdim, gridsize = gsize, type = 'abund'
   )$abund
   
-  this_sp <- "GIROPA"
   expect_error(
     torusonesp.all(
       species = this_sp,
@@ -80,35 +70,20 @@ test_that("fails with a one-species dataset", {
       plotdim = pdim,
       gridsize = gsize
     ), 
-    "two or more species"
+    "Invalid stem density of focal sp per habitat of focal map"
   )
 })
 
-
-
 test_that("passes with a two-species dataset", {
-  # FIXME: Reduce duplication.
-  # Setup
   skip_if_not_installed("pasoh")
-  total_abundance <- 50
-  cns_too_few_sp <- pasoh::pasoh_3spp %>% 
-    filter(sp   %in% c("XERONO", "GIROPA")) %>% 
-    filter(status == "A") %>% 
-    group_by(sp) %>% 
-    sample_n(total_abundance) %>% 
-    ungroup() 
-  hab <- pasoh::pasoh_hab_index20
-  pdim <- c(1000, 500)
-  gsize <- 20
+  
+  cns_2sp <- cns_tiny %>% 
+    filter(sp %in% sample(cns_tiny$sp, 2))
   
   abun_quad <- abundanceperquad(
-    cns_too_few_sp,
-    plotdim = pdim,
-    gridsize = gsize,
-    type = 'abund'
+    cns_2sp, plotdim = pdim, gridsize = gsize, type = 'abund'
   )$abund
   
-  this_sp <- "XERONO"
   expect_silent(
     torusonesp.all(
       species = this_sp,
@@ -120,3 +95,65 @@ test_that("passes with a two-species dataset", {
   )
 })
 
+
+
+# Datasets other than Pasoh -----------------------------------------------
+
+# Small dataset from BCI
+cns_bci <- bciex::bci12t7mini
+sp_top3 <- cns_bci %>%
+  count(sp) %>%
+  arrange(desc(n)) %>%
+  top_n(3) %>%
+  pull(sp)
+
+cns_3sp_bci <- filter(cns_bci, status == "A", sp  %in% sp_top3)
+this_sp_bci <- first(sp_top3)
+
+pdim_bci <- c(1000, 500)
+gsize_bci <- 20
+
+test_that("passes habitat data in bci package but FIXME not with bciex", {
+  skip_if_not_installed("bci")
+  skip_if_not_installed("bciex")
+
+  abun_quad_bci <- abundanceperquad(
+    cns_3sp_bci, plotdim = pdim_bci, gridsize = gsize_bci, type = 'abund'
+  )$abund
+  
+  expect_silent(
+    torusonesp.all(
+      species = this_sp_bci,
+      hab.index20 = bci::bci_habitat,
+      allabund20 = abun_quad_bci,
+      plotdim = pdim_bci,
+      gridsize = gsize_bci
+    )
+  )
+  
+  # FIXME: This should pass but fails
+  hab_bciex <- rename(bciex::bci_habitat, habitats = habitat) %>% 
+    mutate(habitats = as.integer(as.factor(habitats)))
+  expect_silent(
+    torusonesp.all(
+      species = this_sp_bci,
+      hab.index20 = hab_bciex,
+      allabund20 = abun_quad_bci,
+      plotdim = pdim_bci,
+      gridsize = gsize_bci
+    )
+  )
+  
+  hab_random <- mutate(
+    hab_bciex, habitats = sample(1:7, length(habitats), replace = TRUE)
+  )
+  expect_silent(
+    torusonesp.all(
+      species = this_sp_bci,
+      hab.index20 = hab_random,
+      allabund20 = abun_quad_bci,
+      plotdim = pdim_bci,
+      gridsize = gsize_bci
+    )
+  )
+})
--- tmp.R
+++ tmp.R
@@ -1,50 +1,6 @@
-library(dplyr)
+# Todo
 
-cns <- fgeo.data::luquillo_tree6_1ha
+# Ask Russo et al to describe what to do when divisions are invalid. 
+# When would that be the case?
 
-cns %>% 
-  filter(status == "A") %>% 
-  count(sp) %>% 
-  arrange(desc(n)) %>% 
-  top_n(3) %>% 
-  pull(sp)
-
-cns %>% count(sp) %>% fgeo.tool::row_top(n, 3)
-
-cns %>% fgeo.tool::row_top("sp", 3) %>% select(sp)
-
-
-test_that("regression with Luquillo", {
-  # skip_if_not_installed("fgeo.data")
-  cns <- fgeo.data::luquillo_tree6_random
-  hab <- fgeo.data::luquillo_habitat
-  # Via extract_plotdim()
-  # See also https://forestgeo.si.edu/sites/north-america/luquillo
-  pdim <- c(320, 500)
-  # Via extract_gridsize()
-  gsize <- 5
-  abun_quad <- abundanceperquad(
-    cns,
-    plotdim = pdim,
-    gridsize = gsize,
-    type = 'abund'
-  )$abund
-  
-  sp_alive_3most_abun <- c("PREMON", "CASARB", "SLOBER")
-  sp_alive_1most_abun <- sp_alive_abundant[[1]]
-  out_onesp <- torusonesp.all(
-    species = "PREMON",
-    hab.index20 = hab,
-    allabund20 = abun_quad,
-    plotdim = pdim,
-    gridsize = gsize
-  )
-  
-  # one_sp_pasoh <- t(out_onesp)
-  # expect_known_output(
-  #   one_sp_pasoh,
-  #   "ref-one_sp_pasoh",
-  #   print = TRUE,
-  #   update = TRUE
-  # )
-})
+# Test with luquillo and BCI.
