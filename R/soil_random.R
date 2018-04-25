bb8808bdb6a3cff01ff0d77470eeb7776f97c529
maurolepore <maurolepore@gmail.com>
2018-04-25 07:40:47 -0400

    Update website.

--- R/soil_random.R
+++ R/soil_random.R
@@ -4,4 +4,7 @@
 #' `M3Al` has been randomized. It is useful for examples but not for research.
 #'
 #' @source Graham Zemunik (via https://goo.gl/2EwLQJ).
+#' 
+#' @examples 
+#' str(soil_random, give.attr = FALSE)
 "soil_random"
