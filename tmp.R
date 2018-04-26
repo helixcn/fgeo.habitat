47083cece5b23d1468ba24f9b594a7ef2c6ad2e4
maurolepore <maurolepore@gmail.com>
2018-04-25 20:33:35 -0400

    Extend vignette ttt.

--- tmp.R
+++ tmp.R
@@ -1,1 +1,10 @@
-# Include vignette ttt from try.
+# Check links in vignette.
+
+
+
+# From .R send to terminal: control + alt + enter
+git checkout master
+git merge 23_ttt
+git pull
+git push
+git checkout 23_ttt
