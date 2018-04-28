65aa5a3c1e67c8c0d272ecb6aee559e1e4b12ccd
maurolepore <maurolepore@gmail.com>
2018-04-28 09:08:04 -0400

    Make examples on readme more succint.

---
+++ R/abund_index.R
@@ -0,0 +1,27 @@
+#' Count rows by quadrat-index.
+#'
+#' Count rows by quadrat-index. This is a conservative wrapper around the
+#' function `abundanceperquad()` of the CTFSRPackage. Its output is always
+#' abundance (not basal area nor agb) and includes all available rows. If you
+#' want to exclude trees of some particular dbh range you need to do it before
+#' using this function.
+#'
+#' @inheritParams ctfs::abundanceperquad
+#'
+#' @return A dataframe where each quadrat-index is a column and each species
+#' is a rowname.
+#' @export
+#'
+#' @seealso `fgeo.tool::add_index()`.
+#'
+#' @examples
+#' cns <- luquillo_tree6_random
+#' pdm <- c(1000, 500)
+#' gsz <- 20
+#' abund_index(cns, pdm, gsz)
+abund_index <- function(censdata, plotdim, gridsize) {
+  stopifnot(!missing(censdata), !missing(plotdim), !missing(gridsize))
+  abundanceperquad(
+    censdata = censdata, plotdim = plotdim, gridsize = gridsize, mindbh = 0
+  )$abund
+}
