188f1407fda7c1bbc5741f4dbd3f582026d15e26
maurolepore <maurolepore@gmail.com>
2018-05-03 08:32:20 -0400

    Plan to silent krig

--- tmp.R
+++ tmp.R
@@ -1,4 +1,27 @@
-# Remove dependency on purrr::quietly, only function used.
+library(fgeo.habitat)
+
+quiet_krig <- function(krg) {
+  function(...) {
+    output <- capture.output({
+      result <- krg(...)
+    })
+    output <- strsplit(paste0(output, collapse = "\n"), "\n\\$df\n")[[1]][1]
+    message(output)
+    result
+  }
+}
+
+
+
+
+
+# Develop generic as_df to format fgeo output as data frame
+# rename tt_df by as_df
+# rename mat_enframe_ls as list_columns() or gather(<matrix>)
+# raname mat_enframe() of class "tt" as as_df(<tt_df>)
+# krig_df() pull only the df component and rearrange arguments to 
+# krig_df(var, ...) for convenient lapply().
+
 # Make krig safer and more consistent with other functios. 
 # 1. Remove defaults of gridsize and plotdim. And check with Graham if
 # krig_breaks are special for plotdim c(1000, 500) or in general.
@@ -15,3 +38,27 @@ plotdim_y <- plotdim[2]
 # extract_gridsize_soil
 
 # release()
+
+time_it <- function(f) {
+  force(f)
+  function(...) {
+    system.time(f(...))
+  }
+}
+
+
+# List of functions to compare
+library(tidyverse)
+library(fgeo.habitat)
+compare <- list(
+  GetKrigedSoil = function(var) suppressMessages(GetKrigedSoil(soil_fake, var)),
+  krig = function(var)          suppressMessages(krig(soil_fake, var))
+)
+
+call_fun <- function(f, ...) f(...)
+lapply(compare, time_it(call_fun), var = "c")
+
+
+
+
+
